24200ac8c2b63b966b65dff0ee3afe7729a12471
DFettes <DFettes@daniel_fettes@hotmail.com>
2014-05-09 16:22:06 -0400

    baserunner logic

--- src/baseball/Inning.java
+++ src/baseball/Inning.java
@@ -6,20 +6,26 @@ public class Inning {
 		String[] results = {"Single", "Double", "Triple", "HOME RUN!", "Walk", "Hit By Pitch", "Strikeout", "Out", "Ground Out", "Fly Out"};
 		int outs = 0; 
 		int[] runsBatter = {0, BatterUp};
-		boolean run1b = false;
-		boolean run2b = false;
-		boolean run3b = false;
+		Player run1b = null;
+		Player run2b = null;
+		Player run3b = null;
+		Player[] runnersOnBase = {null, null, null};
 		//Team inningTeam = t;
 		
 		
 		System.out.println("Current Team at Bat: " + t.name);
 		while (outs < 3){
-			/*System.out.println("Outs: " + outs);
-			System.out.println("Runs: " + runs);
-			System.out.println("First Base: " + run1b);
-			System.out.println("Second Base: " + run2b);
-			System.out.println("Third Base: " + run3b);
-			*/
+			//runnersOnBase = RunnersOnBase.checkRunners(runnersOnBase);
+			
+			System.out.println("Outs: " + outs);
+			System.out.println("Runs: " + runsBatter[0]);
+			if (run1b !=null){
+			System.out.println("First Base: " + run1b.name);}
+			if (run2b !=null){
+			System.out.println("Second Base: " + run2b.name);}
+			if (run3b !=null){
+			System.out.println("Third Base: " + run3b.name);}
+			
 			
 			Player p1 = t.battingOrder[BatterUp];
 			int resultCode = AtBat.NewAtBat(p1);
@@ -27,9 +33,9 @@ public class Inning {
 			//System.out.println();
 			
 			if (resultCode > 6){
-				if (resultCode == 10 && run3b && outs < 2){
+				if (resultCode == 10 && run3b!=null && outs < 2){
 					double randomSacFly = Math.random();
-					run3b = false;
+					run3b = null;
 					if (randomSacFly < 0.7){
 						runsBatter[0]++;
 						result = (result + " - Sacrifice Fly (SAFE)");
@@ -41,83 +47,93 @@ public class Inning {
 				outs++;
 			}
 			else if (resultCode == 1){		
-				if (run3b){
-					run3b = false;
+				if (run3b!=null){
+					run3b = null;
 					runsBatter[0]++;
 				}
-				if (run2b){
-					run2b = false;
-					run3b = true;
+				if (run2b!=null){
+					run3b = run2b;
+					run2b = null;
 				}
-				if (run1b){
-					run1b = false;
-					run2b = true;
+				if (run1b!=null){
+					run2b = run1b;
+					run1b = null;
 				}
-				run1b = true;
+				run1b = p1;
 			}
 			
 			else if (resultCode == 2){		
-				if (run3b){
-					run3b = false;
+				if (run3b!=null){
+					run3b = null;
 					runsBatter[0]++;
 				}
-				if (run2b){
-					run2b = false;
+				if (run2b!=null){
+					run2b = null;
 					runsBatter[0]++;
 				}
-				if (run1b){
-					run1b = false;
-					run3b = true;
+				if (run1b!=null){
+					run3b = run1b;
+					run1b = null;
 				}
-				run2b = true;
+				run2b = p1;
 			}
 			
 			else if (resultCode == 3){		
-				if (run3b){
-					run3b = false;
+				if (run3b!=null){
+					run3b = null;
 					runsBatter[0]++;
 				}
-				if (run2b){
-					run2b = false;
+				if (run2b!=null){
+					run2b = null;
 					runsBatter[0]++;
 				}
-				if (run1b){
-					run1b = false;
+				if (run1b!=null){
+					run1b = null;
 					runsBatter[0]++;
 				}
-				run3b = true;
+				run3b = p1;
 			}
 			
 			else if (resultCode == 4){		
-				if (run3b){
-					run3b = false;
+				if (run3b!=null){
+					run3b = null;
 					runsBatter[0]++;
 				}
-				if (run2b){
-					run2b = false;
+				if (run2b!=null){
+					run2b = null;
 					runsBatter[0]++;
 				}
-				if (run1b){
-					run1b = false;
+				if (run1b!=null){
+					run1b = null;
 					runsBatter[0]++;
 				}
 				runsBatter[0]++;
 			}
 			
 			else if (resultCode == 5 || resultCode == 6){		
-				if (run1b && run2b && run3b){
+				if (run1b!=null && run2b!=null && run3b!=null){
+					run3b = run2b;
+					run2b = run1b;
+					run1b = p1;
 					runsBatter[0]++;
+				}				
+				else if (run1b!=null && run2b!=null) {
+					run3b = run2b;
+					run2b = run1b;
+					run1b = p1;
 				}
-				else if (run1b && run2b || run1b && run3b || run2b && run3b){
-					run1b = run2b = run3b = true;
+				else if (run2b!=null && run3b!=null){
+					run1b = p1;
 				}
-				else if (run3b || run2b){
-					run1b = true;
+				else if (run1b!=null && run3b!=null) {
+					run2b = run1b;
+					run1b = p1;
 				}
-				else if (run1b){
-					run2b = true;
+				else if (run1b!=null){
+					run2b = run1b;
+					run1b = p1;
 				}
-				else run1b = true;
+				else run1b = p1;
 			}
 			
 			
@@ -127,6 +143,9 @@ public class Inning {
 			}
 			
 			System.out.println(result);
+			runnersOnBase[0] = run1b;
+			runnersOnBase[1] = run2b;
+			runnersOnBase[2] = run3b;
 		}
 		System.out.println();
 		//System.out.println("3 Outs, inning over");
